771438c5903208255fdcbf007a920ed5869d2012
Adrian Cosme-Halverson <adrian.cosme5850@gmail.com>
2022-10-18 18:30:17 +0000

    Added dependencies for redis

--- client.js
+++ client.js
@@ -9,12 +9,3 @@ const socket = io.connect("http://localhost:3002/messages")
 const startTime = new Date();
 const endTime = new Date(startTime.getTime() + 300000)
 
-socket.emit('itemForAuction', {
-    userId: 'Cognito id',
-    itemId: chance.guid(),
-    item:'test item',
-    itemDescription: 'Test item description',
-    startTime: startTime,
-    endTime: endTime,
-    intialBid: 'dollar amount',
-})
