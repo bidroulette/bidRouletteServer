776e8b41cbd0cc201a36e5f509eda05767627c16
DAVE.TUSIA@GMAIL.COM~ <DAVE.TUSIA@GMAIL.COM>
2022-10-20 10:28:28 -0700

    updated

---
+++ hostClient.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const io = require('socket.io-client');
+const Chance = require('chance');
+const chance = new Chance();
+
+const socket = io.connect("http://localhost:3002/messages")
+
+const startTime = new Date();
+const auctionTime =  10;
+
+socket.emit('itemForAuction', {
+    auctionId: chance.guid(),
+    itemId: chance.guid(),
+    item:'test item',
+    itemDescription: 'Test item description',
+    startTime: startTime,
+    auctionTime: auctionTime,
+    currentBid: 'dollar amount',
+    userId: 'test'
+})
+
+socket.on('endAuction', (payload) => {
+  console.log(payload);
+})
