1b759f540140e31f1a7681920fc7e15f54158664
DAVE.TUSIA@GMAIL.COM~ <DAVE.TUSIA@GMAIL.COM>
2022-10-21 08:46:29 -0700

    Final update

--- Live-Terminal-Test/hostClient.js
+++ Live-Terminal-Test/hostClient.js
@@ -7,17 +7,17 @@ const chance = new Chance();
 const socket = io.connect("http://localhost:3002/messages")
 
 const startTime = new Date();
-const auctionTime =  20;
+const auctionTime = 20;
 
 socket.emit('itemForAuction', {
-    auctionId: chance.guid(),
-    itemId: chance.guid(),
-    item:'test item',
-    itemDescription: 'Test item description',
-    startTime: startTime,
-    auctionTime: auctionTime,
-    intialBid: 'dollar amount',
-    userId: 'test'
+  auctionId: chance.guid(),
+  itemId: chance.guid(),
+  item: 'test item',
+  itemDescription: 'Test item description',
+  startTime: startTime,
+  auctionTime: auctionTime,
+  intialBid: 'dollar amount',
+  userId: 'test'
 })
 
 socket.on('endAuction', (payload) => {
--- Live-Terminal-Test/testClient2.js
+++ Live-Terminal-Test/testClient2.js
@@ -8,30 +8,30 @@ let intialBid = 10;
 let intervalId = null;
 
 socket.on('itemReady', (payload) => {
-    socket.emit('joinRoom', {
-        itemId: payload.itemId,
-    })
-    setTimeout(() => makeBid(), 2500)
-    console.log(payload)
+  socket.emit('joinRoom', {
+    itemId: payload.itemId,
+  })
+  setTimeout(() => makeBid(), 2500)
+  console.log(payload)
 })
 
 let makeBid = () => {
-    intervalId = setInterval(() => {
-        if (intialBid >= 50) { stopBid() }
-        else {
-            intialBid += 10;
-            console.log(`Placing bid of ${intialBid}`)
-            socket.emit('bid', { userBid: intialBid, userId: 'Client 2' })
-        }
-    }, 10000);
+  intervalId = setInterval(() => {
+    if (intialBid >= 50) { stopBid() }
+    else {
+      intialBid += 10;
+      console.log(`Placing bid of ${intialBid}`)
+      socket.emit('bid', { userBid: intialBid, userId: 'Client 2' })
+    }
+  }, 10000);
 }
 
 socket.on('endAuction', (payload) => {
-    console.log(payload);
+  console.log(payload);
 })
 
 socket.on('bid', (payload) => {
-    console.log(payload);
+  console.log(payload);
 })
 
 socket.emit('bid', (payload) => {
@@ -39,6 +39,6 @@ socket.emit('bid', (payload) => {
 })
 
 function stopBid() {
-    clearInterval(intervalId);
-    console.log('Personal max bid limit hit, stopping bids');
+  clearInterval(intervalId);
+  console.log('Personal max bid limit hit, stopping bids');
 }
